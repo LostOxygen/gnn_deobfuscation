aa44f39afd86311082f2b4848f5073da59294943
LostOxygen <jonathan.evertz@gmail.com>
2022-06-09 20:13:00 +0200

    added more operations

--- utils/datasets.py
+++ utils/datasets.py
@@ -7,6 +7,16 @@ from torch_geometric.data import Data
 def gen_expr_data() -> Iterator[Data]:
     """
     Helper function to yield a data object for a simple expression like x+y=z
+    0: addition
+    1: subtraction
+    2: multiplication
+    3: division
+    4: logical and
+    5: logical or
+    6: logical xor
+    7: logical not
+    8: logical shift left
+    9: logical shift right
 
     Paramters:
         None
@@ -14,7 +24,7 @@ def gen_expr_data() -> Iterator[Data]:
     Returns:
         Data object for the given dataset
     """
-    operations_list = [0, 1, 2, 3]  # for 0: add, 1: sub, 2: mul, 3: div
+    operations_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  
     edge_index = torch.tensor([
             [0, 1],
             [1, 3],
@@ -37,6 +47,14 @@ def gen_expr_data() -> Iterator[Data]:
                 if y_val == 0:
                     y_val += 1e-8
                 z_val = x_val / y_val
+            case 4: z_val = x_val & y_val
+            case 5: z_val = x_val | y_val
+            case 6: z_val = x_val ^ y_val
+            case 7: 
+                z_val = ~x_val
+                y_val = 0
+            case 8: z_val = x_val << y_val
+            case 9: z_val = x_val >> y_val
 
         x = torch.tensor([[x_val], [0.], [y_val], [z_val]], dtype=torch.float)
         y = torch.tensor([chosen_operation], dtype=torch.long)
