e2a0af031b0ce386c61558b22f3f7b542e34ca20
LostOxygen <jonathan.evertz@gmail.com>
2022-09-13 08:07:00 +0200

    changed back model size

--- utils/models.py
+++ utils/models.py
@@ -8,11 +8,11 @@ from torch_geometric.nn import SAGEConv, GCNConv, Sequential, GATv2Conv
 class GATNetwork(torch.nn.Module):
     """Graph attention neural network"""
 
-    def __init__(self, dim_in, dim_h, dim_out, heads=8):
+    def __init__(self, dim_in, dim_h, dim_out, heads=12):
         super().__init__()
         self.gat1 = GATv2Conv(dim_in, dim_h, heads=heads)
-        # self.gat2 = GATv2Conv(dim_h*heads, dim_h, heads=heads)
-        # self.gat3 = GATv2Conv(dim_h*heads, dim_h, heads=heads)
+        self.gat2 = GATv2Conv(dim_h*heads, dim_h, heads=heads)
+        self.gat3 = GATv2Conv(dim_h*heads, dim_h, heads=heads)
         self.gat4 = GATv2Conv(dim_h*heads, dim_out, heads=1)
 
     def forward(self, x, edge_index):
@@ -20,13 +20,13 @@ class GATNetwork(torch.nn.Module):
         h = self.gat1(x, edge_index)
         h = F.elu(h)
 
-        # h = F.dropout(h, p=0.2, training=self.training)
-        # h = self.gat2(h, edge_index)
-        # h = F.elu(h)
+        h = F.dropout(h, p=0.2, training=self.training)
+        h = self.gat2(h, edge_index)
+        h = F.elu(h)
 
-        # h = F.dropout(h, p=0.2, training=self.training)
-        # h = self.gat3(h, edge_index)
-        # h = F.elu(h)
+        h = F.dropout(h, p=0.2, training=self.training)
+        h = self.gat3(h, edge_index)
+        h = F.elu(h)
 
         h = F.dropout(h, p=0.2, training=self.training)
         h = self.gat4(h, edge_index)
